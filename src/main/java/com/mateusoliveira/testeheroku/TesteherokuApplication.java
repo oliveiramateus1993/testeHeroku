fd0a59c0937301a3679876d23ffe399f5a256585
Mateus Oliveira <oliveira.mateus1993@gmail.com>
2018-03-25 14:38:42 -0300

    sprin web teta

--- src/main/java/com/mateusoliveira/testeheroku/TesteherokuApplication.java
+++ src/main/java/com/mateusoliveira/testeheroku/TesteherokuApplication.java
@@ -2,9 +2,18 @@ package com.mateusoliveira.testeheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class TesteherokuApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello Word!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteherokuApplication.class, args);
