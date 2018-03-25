520608641892071d4bb71ea35096db0d9a6e68d4
Mateus Oliveira <oliveira.mateus1993@gmail.com>
2018-03-25 14:28:10 -0300

    second commit

---
+++ src/main/java/com/mateusoliveira/testeheroku/TesteherokuApplication.java
@@ -0,0 +1,12 @@
+package com.mateusoliveira.testeheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteherokuApplication.class, args);
+	}
+}
